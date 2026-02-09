b19f995eaf8caeb090525e205398eaf53673e416
luffy <118557832+kaustav812004@users.noreply.github.com>
2026-02-09 15:23:57 +0530

    Change sum calculation to sum of numbers from 1 to n
    Updated the logic to calculate the sum of numbers from 1 to n instead of just the sum of digits.

--- Lecture 3/sumofdigit.cpp
+++ Lecture 3/sumofdigit.cpp
@@ -4,11 +4,16 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-
+    // cout<<n*(n+1)/2<<endl;
+    // return 0;
     int sum = 0;
-    while(n > 0) {
-        sum += n % 10;
-        n /= 10;
+    // for(int i = 1; i <= n; i++){
+    //     sum += i;
+    // }
+    int i = 1;
+    while(i <= n){
+        sum+=i;
+        i++;
     }
 
     cout << sum;
