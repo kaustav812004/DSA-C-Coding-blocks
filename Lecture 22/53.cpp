8b41fc54b9b71ae4ed2fb321184647f0ace43588
luffy <118557832+kaustav812004@users.noreply.github.com>
2026-03-09 11:26:08 +0530

    Add files via upload

---
+++ Lecture 22/53.cpp
@@ -0,0 +1,12 @@
+int kadanes(vector<int>& arr){
+    int n = arr.size();
+    int sum = 0;
+    int maxsum = INT_MIN;
+    for(int i = 0; i < n; i++){
+        sum+=arr[i];
+        maxsum = max(sum, maxsum);
+        if(sum < 0) sum = 0;
+    }
+    return maxsum;
+}
+
