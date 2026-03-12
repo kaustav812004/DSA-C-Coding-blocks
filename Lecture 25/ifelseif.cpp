db2d7b3e90a7df93ff51e6219c50519274f154fc
luffy <118557832+kaustav812004@users.noreply.github.com>
2026-03-12 11:33:20 +0530

    Add if-else statement example in ifelseif.cpp

---
+++ Lecture 25/ifelseif.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10, b = 20;
+    if(a < b){
+        cout<<"1"<<endl;
+        a+=30;
+    }
+    // if(a > b){
+    //     cout<<"2"<<endl;
+    // }
+    else if(a > b){
+        cout<<"2"<<endl;
+    }
+}
